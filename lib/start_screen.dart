8cc9fe5872c5da03397506c41605f79051110d21
Juan Pablo <juanpablo@meowtel.com>
2023-05-08 11:55:46 -0300

    oppacity

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(147, 255, 255, 255),
           ),
           const SizedBox(height: 50),
           const Text(
@@ -22,12 +23,15 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () => {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text(
+            icon: const Icon(
+              Icons.arrow_right_alt,
+            ),
+            label: const Text(
               'Start Quiz',
             ),
           ),
